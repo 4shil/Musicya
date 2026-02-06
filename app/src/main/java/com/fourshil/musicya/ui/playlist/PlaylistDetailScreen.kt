f431b5a54d2afb3e91efcd342b520a401d695a5c
4shil <166588383+4shil@users.noreply.github.com>
2026-02-06 12:00:00 +0530

    Remove unused imports from PlaylistDetailScreen

--- app/src/main/java/com/fourshil/musicya/ui/playlist/PlaylistDetailScreen.kt
+++ app/src/main/java/com/fourshil/musicya/ui/playlist/PlaylistDetailScreen.kt
@@ -1,7 +1,6 @@
 package com.fourshil.musicya.ui.playlist
 
 import android.net.Uri
-import androidx.compose.ui.graphics.Color
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
